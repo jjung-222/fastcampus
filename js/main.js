888e81b5e92d5cf737817c9c77898f071a776b55
jjung-222 <lyj7877@naver.com>
2023-08-02 23:33:33 +0900

    section & menu commit

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ searchInputEl.addEventListener('blur', function(){
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 })
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+    console.log(window.scrollY);
+    if(window.scrollY > 500) {
+        // badgeEl.style.display = 'none'
+        gsap.to(badgeEl, 0.6, {
+            opacity: 0,
+            display: 'none'
+        }) //요소, 지속시간(초), 옵션
+    } else {
+        // badgeEl.style.display = 'block'
+        gsap.to(badgeEl, 0.6, {
+            opacity: 1,
+            display: 'block'
+        }) //요소, 지속시간(초), 옵션
+    }
+}, 300))
